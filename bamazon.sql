e6aaf6ff633ced8a406cf47d45c8627cbe4ce29d
Hannah Jacoby <hjacoby@grubhub.com>
2019-08-30 10:04:28 -0600

    db setup

---
+++ bamazon.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE bamazon; 
+
+USE bamazon; 
+
+CREATE TABLE products(
+    item_id INT NOT NULL, 
+    product_name VARCHAR(45) NULL, 
+    department_name VARCHAR(45) NULL, 
+    price INT NOT NULL, 
+    stock_quantity INT NOT NULL
+); 
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("1, apples, produce, $1, 5");
